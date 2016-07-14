5d5e2e144e08b973f696bbf5d7089747fb27d70a
Brian Cox <therealbriancox@gmail.com>
2016-07-13 23:10:32 -0400

    Initial Commit. Empty single page app

---
+++ PokemonChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonChat
+//
+//  Created by ----- --- on 7/13/16.
+//  Copyright © 2016 PokemonGoTeamChat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
